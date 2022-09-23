881e6fb7ef1567a8e35d0b5bebdc894fa5ceb034
DaryaPark <89138103+DaryaPark@users.noreply.github.com>
2022-09-23 20:01:49 +0300

    the problem with del

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,25 @@ const autoprefixer = require('gulp-autoprefixer')
 const cleanCSS = require('gulp-clean-css')
 const svgSprite = require('gulp-svg-sprite')
 const image = require('gulp-image')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify-es').default
+const notify = require('gulp-notify')
+const sourcemaps = require('gulp-sourcemaps')
+const del = require('del')
 const browserSync = require('browser-sync').create()
+
+const clean = () => {
+    return del(['dist'])
+}
+
+const resources = () => {
+    return src('src/resources/**')
+    .pipe(dest('dist'))
+}
 
 const styles = () => {
     return src('src/styles/**/*.css')
+     .pipe(sourcemaps.init())
      .pipe(concat('main.css'))
      .pipe(autoprefixer({
         cascade:false
@@ -16,6 +31,7 @@ const styles = () => {
     .pipe(cleanCSS({
         level:2
     }))
+     .pipe(sourcemaps.write())
      .pipe(dest('dist'))
      .pipe(browserSync.stream())
 }
@@ -44,6 +60,24 @@ const svgSprites = () => {
 
 }
 
+const scripts = () => {
+    return src([
+        'src/js/components/**/*.js',
+        'src/js/main.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('app.js'))
+    .pipe(uglify({
+        toplevel: true
+    }).on('error', notify.onError))
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist'))
+    .pipe(browserSync.stream())
+}
+
 const watchFiles = () => {
     browserSync.init({
         server:{
@@ -69,7 +103,10 @@ const images = () => {
 watch('src/**/*.html', htmlMinify)
 watch('src/styles/**/*.css', styles)
 watch('src/images/svg/**/*.svg', svgSprites)
+watch('src/js/**/*.js', scripts)
+watch('src/resources/**', resources)
 
-exports.styles = styles;
+exports.styles = styles
+exports.scripts = scripts
 exports.htmlMinify = htmlMinify
-exports.default = series(htmlMinify, styles, images, svgSprites, watchFiles)
+exports.default = series(clean, resources, htmlMinify, scripts, styles, images, svgSprites, watchFiles)
